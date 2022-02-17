eaac48d4e4f49bfdb01be3e580645d101d6905e2
ugurcsen <trmaster101@gmail.com>
2022-02-17 15:36:46 +0300

    Monty hall home work completed.

---
+++ Monty_Hall_Problem/main.py
@@ -0,0 +1,32 @@
+import random
+
+win = 0
+for i in range(10000):
+    gates = [0, 0, 0]
+    gates[random.randint(0, 2)] = 1
+    choose = random.randint(0, 2)
+
+    if gates[choose] == 1:
+        win += 1
+
+print(win * 100 / 10000)
+
+win = 0
+for i in range(10000):
+    gates = [0, 0, 0]
+    gates[random.randint(0, 2)] = 1
+    fChoose = random.randint(0, 2)
+    secChoose = 0;
+    while True:
+        sChoose = random.randint(0, 2)
+        if sChoose != fChoose and gates[sChoose] == 0:
+            break
+
+    for x in range(3):
+        if x != sChoose and x != fChoose:
+            secChoose = x
+
+    if gates[secChoose] == 1:
+        win += 1
+
+print(win * 100 / 10000)
